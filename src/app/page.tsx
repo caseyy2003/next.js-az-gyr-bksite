ae2ef41c238c42c863c08985fd68de02ccbe5a90
Casey Yontz <caseyy33@gmail.com>
2025-04-12 12:23:55 -0700

    chagned some verbiage - experiemetning with Semrush

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://myazlawfirm.com/" />
         {/* Open Graph for social sharing */}
-        <meta property="og:title" content="Arizona Bankruptcy Attorney | Consultations" />
+        <meta property="og:title" content="Wherever you are in Arizona, we have an experienced bankruptcy lawyer near you." />
         <meta property="og:description" content="Bankruptcy services in Arizona. Free evaluation for Chapter 7 & Chapter 13 bankruptcy." />
         <meta property="og:url" content="https://myazlawfirm.com/" />
         <meta property="og:type" content="website" />
@@ -24,16 +24,16 @@ const HomePage = () => {
         <h1 className="text-3xl font-bold mb-4">Bankruptcy Lawyer in Phoenix - Serving All of Arizona</h1>
         <h3 className="text-xl mb-4"></h3>
         <p className="text-lg mb-6">
-          Struggling with debt? We offer expert bankruptcy consultations for Chapter 7 & Chapter 13—all handled virtually. Get a free evaluation today.
+          Struggling with debt? We offer expert bankruptcy consultations for Chapter 7 & Chapter 13 everywhere in Arizona. Get a free evaluation today.
         </p>
 
         {/* Lead Generation Form */}
         <LeadCaptureForm />
 
         {/* Service Areas */}
-        <h2 className="text-2xl font-semibold mt-12">Is There a Bankruyptcy Lawyer Near Me? Yes. We Serve All of Arizona</h2>
+        <h2 className="text-2xl font-semibold mt-12">Asking, is thre a high-quality a bankruyptcy Lawyer Near Me? Yes. We Serve All of Arizona</h2>
         <br />
-        <h4 className="text-lg mb-4">Asking is there a bankruptcy lawyer near me? Whether you are looking for a bankruptcy lawyer in Phoenix or anywhere in Ariozna, we have you covered. We serve all of Arizona. </h4>
+        <h4 className="text-lg mb-4">Whether you are looking for a bankruptcy attorney in Phoenix or anywhere in Ariozna, we have you covered. We serve all of Arizona. </h4>
         
         <ul className="grid grid-cols-2 md:grid-cols-3 gap-2">
           {serviceAreas.map((area) => (
